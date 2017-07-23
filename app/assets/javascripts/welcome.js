022270cc7da1edca06572a15fcd77a6a2b95d8f0
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-07-23 12:52:16 +0200

    bind cover slide up to scroll

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,19 @@
-$(document).on("click", "div#splash", function(e){
-   $('.cover').addClass('slideOutUp');
+// where the magic happens
+var slideCoverUp =  _.once(function() {
+  $('.cover').addClass('slideOutUp');
 });
+
+// bind to splash logo click
+$(document).ready(function() {
+  $("div#splash").click(function() {
+     slideCoverUp();
+  });
+});
+
+// bind to mouse scroll
+var isFirefox = (/Firefox/i.test(navigator.userAgent));
+var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+window.addEventListener(mousewheelEvent, _.throttle(slideCoverUp, 60), false);
+
+// bind to touchscreen scroll
+window.addEventListener('touchmove', _.throttle(slideCoverUp, 60), false);
