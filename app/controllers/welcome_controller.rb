3c258a32a728ee84177a05b8ec81b2ea89f5163c
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-07-29 20:32:12 +0200

    expose yaml data to welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    event_info = CONTENT
+    @team = event_info[:team]
+    @performance = event_info[:performance]
+    @music = event_info[:music]
   end
 end
