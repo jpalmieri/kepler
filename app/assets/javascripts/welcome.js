dda44cddcea35e16fd78048efcabf7373b9dde24
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-08-02 23:00:41 +0200

    don't invert x and y axes for parallax.js on mobile

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -35,7 +35,18 @@ $(document).ready(function() {
 
   // parallax js magic
   var scene = $('#scene').get(0);
-  var parallax = new Parallax(scene);
+  // mobile User Agent detection
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // instatiate without inverting x and y axes
+    // I like this better on mobile
+    var parallax = new Parallax(scene, {
+      invertX: false,
+      invertY: false
+    });
+  } else {
+    // instatiate as default (x and y inverted)
+    var parallax = new Parallax(scene);
+  }
 
   // temporary action to show body content
   $('.category-link').click(function(e) {
