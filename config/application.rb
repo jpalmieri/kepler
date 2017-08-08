5b9fafbc0690104e5b35f375f1aa851f78f8871f
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-08-08 14:09:43 +0200

    add fonts to production assets

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Kepler
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
