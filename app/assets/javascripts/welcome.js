ae31db83167f1b2ed66661e1e6dd8bafeab377a6
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-08-15 14:21:37 +0200

    add welcome index links

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // hide cover image when user clicks splash image
   $('.cover #splash').click(function() {
     $('.cover').hide();
+    $('.index').show();
   });
 
 });
