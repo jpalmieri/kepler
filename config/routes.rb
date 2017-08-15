517dd4a59aec11210a008c6f2de36fbba2652381
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-08-15 20:45:18 +0200

    add header and nav, start about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'welcome/index'
+  get 'about/index'
 
   resources :team_members, only: [:index, :show]
   resources :performers, only: [:index, :show]
