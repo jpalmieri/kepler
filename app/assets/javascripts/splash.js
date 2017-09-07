8abe56a15bccd739a99a5887f6803819b6dd3c83
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-09-07 13:13:49 +0200

    convert splash init js to object literal pattern

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -1,27 +1,39 @@
-var bindToScroll = function(func) {
-  // bind to mouse scroll
-  var isFirefox = (/Firefox/i.test(navigator.userAgent));
-  var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-  window.addEventListener(mousewheelEvent, _.throttle(function() {
-    func();
-  }, 60), false);
-  // bind to touchscreen scroll
-  window.addEventListener('touchmove', _.throttle(function() {
-    func();
-  }, 60), false);
+var splash = {
+  init: function() {
+    splash.$el = $('.splash');
+    splash.$cta = $('.splash #logo');
+
+    splash.setup();
+  },
+
+  setup: function() {
+    splash.$cta.click(function() {
+      splash.slideUp();
+    });
+
+    splash.bindToScroll(function() {
+      splash.slideUp();
+    });
+  },
+
+  slideUp: function() {
+    splash.$el.addClass('active');
+  },
+
+  bindToScroll: function(func) {
+    // bind to mouse scroll
+    var isFirefox = (/Firefox/i.test(navigator.userAgent));
+    var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+    window.addEventListener(mousewheelEvent, _.throttle(function() {
+      func();
+    }, 60), false);
+    // bind to touchscreen scroll
+    window.addEventListener('touchmove', _.throttle(function() {
+      func();
+    }, 60), false);
+  }
 };
 
 $(document).ready(function() {
-  // Slide splash screen up
-  var slideSplash = function() {
-    $('.splash').addClass('active');
-  };
-
-  $('.splash #logo').click(function() {
-    slideSplash();
-  });
-
-  bindToScroll(function() {
-    slideSplash();
-  });
+  splash.init();
 });
