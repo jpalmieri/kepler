73a59455be5b66760f830639bb7d18ba48bb000f
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-03-15 12:50:37 -0700

    convert to hugo project, move files into their new homes (except views, which will be converted to templates and partials) https://github.com/netlify/victor-hugo scss and sass will need to be converted to postcss javascripts will need to be imported into app.js, import vendor scripts as node modules

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def bio_photo(photo_name)
-    begin
-      image_tag("bio/#{photo_name}")
-    rescue Sprockets::Rails::Helper::AssetNotFound
-      return nil
-    end
-  end
-end
